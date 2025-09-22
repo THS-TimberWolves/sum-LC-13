4175566c46111d3f6cc47d5a08b4e24eb5243b80
LC-13 <allerell000@alpinesd.org>
2025-09-22 11:16:05 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
 #while summation code following directions from Readme
+x = int(input("Enter a number"))
+looped_times= 0
+total = 0
+while looped_times <= x:
+    print(looped_times)
+    looped_times = looped_times + 1
+    total=total+x
+print(total)
